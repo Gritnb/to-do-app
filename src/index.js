5bedd9aec562514604beaa8a01b94b202aa3b7d4
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-10 20:30:15 +0200

    Change theme and new background

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ console.log('works')
 const todo1 = Todo('First', 'Your First Task', '12-04-2025', 'Important', ['1', '2', '3'])
 
 console.log(todo1)
+
+const theme = document.getElementById("theme")
+
+theme.addEventListener('click', changeTheme)
+
+function changeTheme() {
+    const root = document.documentElement
+
+    const newTheme = root.className === "dark" ? "light" : "dark"
+    root.className = newTheme
+}
