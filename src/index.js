ea7c95a5790a92b026ca0fb9269021d5d88ad05b
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-10 21:49:23 +0200

    header hero section complete

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ const todo1 = Todo('First', 'Your First Task', '12-04-2025', 'Important', ['1', 
 
 console.log(todo1)
 
-const theme = document.getElementById("theme")
+const theme = document.getElementById("theme-button")
 
 theme.addEventListener('click', changeTheme)
 
 function changeTheme() {
     const root = document.documentElement
+    const lightIcon = document.getElementById('light-icon')
+    const darkIcon = document.getElementById('dark-icon')
 
     const newTheme = root.className === "dark" ? "light" : "dark"
+
+    if (newTheme === "light") {
+        darkIcon.classList.remove("inactive")
+        lightIcon.classList.add("inactive")
+    } else {
+        lightIcon.classList.remove("inactive")
+        darkIcon.classList.add("inactive")
+    }
     root.className = newTheme
 }
