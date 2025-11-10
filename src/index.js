cf4af026dc0f730f4d0338da63b684afa1614f67
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-10 19:11:30 +0200

    Todo object draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styles.css'
+import Todo from './todo'
 
 console.log('works')
+const todo1 = Todo('First', 'Your First Task', '12-04-2025', 'Important', ['1', '2', '3'])
+
+console.log(todo1)
---
+++ src/todo.js
@@ -0,0 +1,9 @@
+export default function Todo(title, description, due, priority, notes) {
+    return {
+        title,
+        description,
+        due,
+        priority,
+        notes
+    }
+}
