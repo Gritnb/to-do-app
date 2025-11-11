ae43b0bd551ab347f8cae4997f469f5156ab215c
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-11 13:00:33 +0200

    today page draft

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import "./styles.css"
 import changeTheme from "./utils/changeTheme"
-import Todo from './todo'
+import today from "./pages/today"
 
 (function display() {
-
+    // Hooks
+    const content = document.getElementById("content")
     const theme = document.getElementById("theme-button")
+    const navItems = Array.from(document.querySelectorAll(".nav-btn"))
+    const todayButton = document.getElementById("view-today")
 
+    // Listeners
+    // TESTER
+    window.addEventListener('load', today)
+    // TESTER
     theme.addEventListener('click', changeTheme)
+    navItems.forEach(item => {
+        item.classList.remove("selected")
+    })
+
+    todayButton.addEventListener('click', (event) => {
+        event.currentTarget.classList.add("selected")
+        today()
+    })
 })()
 
 
---
+++ src/pages/today.js
@@ -0,0 +1,10 @@
+export default function today() {
+    const content = document.getElementById("content")
+    content.innerHTML = ``;
+
+    // Test
+
+    const header = document.createElement('h1')
+    header.textContent = `THIS IS A TEST`
+    content.append(header)
+}
