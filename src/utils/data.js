ccc9c9d16af623ace0721995352cd9241f900b4e
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-12 21:21:43 +0200

    Add test data.

---
+++ src/utils/data.js
@@ -0,0 +1,10 @@
+import Todo from "../todo"; 
+
+export const userData = {
+    projects: [],
+    todos: [
+        {
+            test: (new Todo("Welcome", "It's your first task", "now", "urgent", "test"))
+        }   
+    ]
+}
