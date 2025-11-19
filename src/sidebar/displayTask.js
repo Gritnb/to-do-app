835847d33437ccbf9af867fe758c7298524dbb40
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-19 15:22:23 +0200

    edit button used as clone

--- src/sidebar/displayTask.js
+++ src/sidebar/displayTask.js
@@ -13,10 +13,22 @@ export default function displayTask(task) {
     task.classList.add("selected")
     
     const taskToDisplay = user.getTask(task.id)
-
+    // Utility DOM Elements
     const taskDivider = document.createElement("hr")
     taskDivider.className = "single-divider"
     
+    const editButton = document.createElement("button")
+    editButton.className = "misc-btn-change"
+    editButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" 
+             viewBox="0 0 24 24"
+             class="icon-small">
+             <title>Edit</title>
+             <path fill="var(--icon-color)"
+                   d="M18.13 12L19.39 10.74C19.83 10.3 20.39 10.06 21 10V9L15 3H5C3.89 3 3 3.89 3 5V19C3 20.1 3.89 21 5 21H11V19.13L11.13 19H5V5H12V12H18.13M14 4.5L19.5 10H14V4.5M19.13 13.83L21.17 15.87L15.04 22H13V19.96L19.13 13.83M22.85 14.19L21.87 15.17L19.83 13.13L20.81 12.15C21 11.95 21.33 11.95 21.53 12.15L22.85 13.47C23.05 13.67 23.05 14 22.85 14.19Z" />
+        </svg>
+    `
+    // Utility DOM Elements
     const container = document.createElement("div")
     container.className = "single-task"
 
@@ -88,18 +100,8 @@ export default function displayTask(task) {
     infoContainer.append(buttonsContainer)
 
     // Change Title Test DOM
-    const changeTitle = document.createElement("button")
-    changeTitle.className = "misc-btn-change"
-    changeTitle.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" 
-             viewBox="0 0 24 24"
-             class="icon-small">
-             <title>Edit</title>
-             <path fill="var(--icon-color)"
-                   d="M18.13 12L19.39 10.74C19.83 10.3 20.39 10.06 21 10V9L15 3H5C3.89 3 3 3.89 3 5V19C3 20.1 3.89 21 5 21H11V19.13L11.13 19H5V5H12V12H18.13M14 4.5L19.5 10H14V4.5M19.13 13.83L21.17 15.87L15.04 22H13V19.96L19.13 13.83M22.85 14.19L21.87 15.17L19.83 13.13L20.81 12.15C21 11.95 21.33 11.95 21.53 12.15L22.85 13.47C23.05 13.67 23.05 14 22.85 14.19Z" />
-        </svg>
-    `
-
+    const changeTitle = editButton.cloneNode(true)
+    
     taskTitle.append(changeTitle)
 
     const changeContainer = document.createElement("div")
