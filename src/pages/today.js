55cb5652b13f992e8a01eec18035e9eb90335e5b
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-23 16:29:13 +0200

    add delete and complete buttons

--- src/pages/today.js
+++ src/pages/today.js
@@ -1,15 +1,14 @@
 import { user } from "../userData/userData"
-import { isToday, parse } from "date-fns"
+import { isToday, parse, format } from "date-fns"
 import { colors } from "../utils/colors"
+import syncData from "../userData/syncData"
 import displayTask from "../sidebar/displayTask"
+import displayMyTasks from "../sidebar/myTasks"
 
 export default function today() {
-    // const todayTasks = user.data.tasks.filter(task => {
-    //     return isToday(parse(task.date, "d MMMM yyyy HH:mm", new Date()))
-    // })
-
-    // Empty today test data
-    const todayTasks = []
+    const todayTasks = user.data.tasks.filter(task => {
+        return isToday(parse(task.date, "d MMMM yyyy HH:mm", new Date()))
+    })
 
     const content = document.getElementById("content")
     content.innerHTML = ``
@@ -45,7 +44,6 @@ export default function today() {
     cardsContainer.className = "today-container"
 
     if (todayTasks.length === 0) {
-
         const icon = document.createElement("p")
         icon.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" 
@@ -76,21 +74,61 @@ export default function today() {
 
     todayTasks.forEach(task => {
         const card = document.createElement("div")
-        card.id = task.id
         card.className = "card"
         card.style.borderLeft = `5px solid ${colors[task.priority]}`
+
+        const infoContainer = document.createElement("div")
+        infoContainer.className= "card-info"
+        infoContainer.id = task.id
 
         const title = document.createElement("h2")
         title.textContent = `${task.title}`
         
         const description = document.createElement("p")
-        description.className = "single-description"
+        description.className = "info-description"
         description.textContent = `${task.description}`
 
-        // Add erase - complete buttons
+        const due = document.createElement("p")
+        due.textContent = `Due: ${format(task.date, "HH:mm")}`
 
-        card.append(title)
-        card.append(description)
+        infoContainer.append(title)
+        infoContainer.append(description)
+        infoContainer.append(due)
+
+        const buttonsContainer = document.createElement("div")
+        buttonsContainer.className = "misc-btn-container"
+
+        const deleteTask = document.createElement("button")
+        deleteTask.className = "misc-btn delete-task"
+        deleteTask.id = `${task.id}`
+        deleteTask.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24"
+                class="icon-small">
+                <title>Delete</title>
+                <path fill="var(--icon-color)"
+                      d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
+            </svg>
+        `
+
+        const completeTask = document.createElement("button")
+        completeTask.className = "misc-btn complete-task"
+        completeTask.id = `${task.id}`
+        completeTask.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24"
+                class="icon-small">
+                <title>Complete</title>
+                <path fill="var(--icon-color)"
+                      d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+            </svg>
+        `
+
+        buttonsContainer.append(deleteTask)
+        buttonsContainer.append(completeTask)
+
+        card.append(infoContainer)
+        card.append(buttonsContainer)
         cardsContainer.append(card)
     })
 
@@ -98,10 +136,20 @@ export default function today() {
     container.append(cardsContainer)
     content.append(container)
 
-    Array.from(document.querySelectorAll(".card"))
+    Array.from(document.querySelectorAll(".card-info"))
         .forEach(card => {
             card.addEventListener("click", (event) => {
                 displayTask(event.currentTarget)
+            })
+        })
+
+    Array.from(document.querySelectorAll('.misc-btn'))
+        .forEach(button => {
+            button.addEventListener("click", (event) => {
+                user.removeTask(event.currentTarget.id)
+                syncData()
+                today()
+                displayMyTasks()
             })
         })
 }
