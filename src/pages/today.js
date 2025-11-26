c59c8c7172256dfaa16ba33f49bd35841c9ca2ce
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-26 23:45:32 +0200

    tasks appear sorted by default

--- src/pages/today.js
+++ src/pages/today.js
@@ -1,7 +1,10 @@
 import { user } from "../userData/userData"
+import FullToMs from "../utils/fullDateToMs"
 import taskCards from "../components/tasksCards"
 
 export default function today() {
-    const todayTasks = user.getTodayTasks()
+    const todayTasks = user.getTodayTasks().sort((a, b) => {
+        return FullToMs(a.date) - FullToMs(b.date)
+    })    
     taskCards(todayTasks, "today")
 }
--- src/pages/upcoming.js
+++ src/pages/upcoming.js
@@ -1,7 +1,10 @@
 import { user } from "../userData/userData"
+import FullToMs from "../utils/fullDateToMs"
 import taskCards from "../components/tasksCards"
 
 export default function upcoming() {
-    const upcoming = user.getUpcomingTasks()
+    const upcoming = user.getUpcomingTasks().sort((a, b) => {
+        return FullToMs(a.date) - FullToMs(b.date)
+    })
     taskCards(upcoming, "upcoming")
 }
