643d0efc1f80acae6e0416a960916c3f989257bf
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-11-29 16:43:34 +0200

    Recheck default options

--- src/handlers/handleClear.js
+++ src/handlers/handleClear.js
@@ -1,4 +1,7 @@
 export default function handleClear() {
     const form = document.querySelector(".new-task")
     form.reset()
+    //Recheck the default options
+    document.getElementById("task-type-id").checked = true
+    document.getElementById("low-prio-id").checked = true
 }
